daece346ee61e5245dd9de129e78f1395ecfec1e
Enrique Cruz <enrique.cs095@unitec.edu>
2021-02-04 20:21:32 -0600

    ejercicio asincrono

---
+++ Ejercicio-Programacion-asincrona-master/Ejercicio-Programacion-asincrona-master/SocialMediaApp.Console/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using SocialMediaApp.Console.Repositories;
+
+namespace SocialMediaApp.Console
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            System.Console.WriteLine("Loading users");
+            LoadUsers();
+            System.Console.WriteLine("Choose user to get posts: ");
+            var userId = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("Loading user posts");
+            GetUserPostsWithComments(userId);
+            stopWatch.Stop();
+            var ts = stopWatch.Elapsed;
+            System.Console.WriteLine($"Completed in: {ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}");
+        }
+
+        private static async Task GetUserPostsWithComments(int userId)
+        {
+            var postRepository = new PostRepository();
+            var commentRepository = new CommentRepository();
+            var posts = postRepository.GetByUserId(userId);
+
+            await Task.Run(() =>
+            {
+                foreach (var post in posts)
+             {
+                var comments = commentRepository.GetByPostId(post.Id);
+                System.Console.WriteLine("-------------------Post--------------------");
+                System.Console.WriteLine($"{post.Title} - {post.Body}");
+                System.Console.WriteLine("-------------------Comments--------------------");
+
+
+                foreach (var comment in comments)
+                {
+                    System.Console.WriteLine("-------------------Comment--------------------");
+                    System.Console.WriteLine($"{comment.Name}: {comment.Body}");
+                    System.Console.WriteLine("-------------------Comment--------------------");
+                        Task.Delay(100).Wait();
+                 }
+
+                }
+            });
+
+        }
+
+        private static async void LoadUsers()
+        {
+            var userRepository = new UserRepository();
+            var users = userRepository.Get();
+            System.Console.WriteLine("Getting users");
+            await Task.Run(() =>
+            {
+                foreach (var user in users)
+            {
+                System.Console.WriteLine($"{user.Id} - {user.Name} - {user.UserName}");
+            }
+
+            });
+        }
+    }
+}
---
+++ Ejercicio-Programacion-asincrona-master/Ejercicio-Programacion-asincrona-master/SocialMediaApp.Console/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SocialMediaApp.Console.Models;
+
+namespace SocialMediaApp.Console.Repositories
+{
+    public class PostRepository
+    {
+        private const string Url = "https://jsonplaceholder.typicode.com/posts";
+        public IReadOnlyList<Post> GetByUserId(int userId)
+        {
+            Task.Delay(8000).Wait();
+            var client = new WebClient();
+
+            var content = client.DownloadString($"{Url}/?userId={userId}");
+
+            return JsonConvert.DeserializeObject<IReadOnlyList<Post>>(content);
+        }
+    }
+}
